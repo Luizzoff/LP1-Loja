4dee893eaf6079be13a83448f10f3a36cb29a0b9
Luiz Gustavo R. Scatalon <145051602+Luizzoff@users.noreply.github.com>
2024-05-12 22:42:10 -0300

    Add files via upload
    Signed-off-by: Luiz Gustavo R. Scatalon <145051602+Luizzoff@users.noreply.github.com>

---
+++ Projeto/Backend/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+const host = "0.0.0.0";
+const porta = 3000;
+
+app.use(express.static("../../Frontend"));
+
+app.listen(porta, host, () => {
+    console.log(`Servidor escutando em http://localhost:${porta}/templates/index.html`)
+});
